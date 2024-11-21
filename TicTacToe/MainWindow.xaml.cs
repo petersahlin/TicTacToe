306c0e52892b06bc938e2b55f338b8d4cf41609a
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-11-21 16:21:54 +0100

    Add project files.

---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
