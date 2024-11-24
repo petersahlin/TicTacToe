63a5b343cc3f2ae329927e580b3a714e9c762b1a
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-11-24 12:10:23 +0100

    CheckForWin works. Fully working Tic Tac Toe

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -9,6 +10,7 @@ namespace TicTacToe
     {
 
         public List<string> AvailableCells { get; set; } = new List<string>();
+        public List<string> Moves { get; set; } = new List<string>();
 
         public MainWindow()
         {
@@ -22,15 +24,18 @@ namespace TicTacToe
         {
             try
             {
-              
+
                 Button1A.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button1A");
+                var checkWin = CheckForWin("1AX");                                  //send button here?
+                if (!checkWin)
+                {
+                    await OpponentsMove("Button1A");
+
+                }
 
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show($"An error occurred: {ex.Message}");
             }
         }
@@ -40,8 +45,11 @@ namespace TicTacToe
             try
             {
                 Button1B.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button1B");
+                var checkWin = CheckForWin("1BX");
+                if (!checkWin)
+                {
+                    await OpponentsMove("Button1B");
+                }
 
             }
             catch (Exception ex)
@@ -56,8 +64,11 @@ namespace TicTacToe
             try
             {
                 Button1C.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button1C");
+                var checkWin = CheckForWin("1CX");
+                if (!checkWin)
+                {
+                    await OpponentsMove("Button1C");
+                }
 
             }
             catch (Exception ex)
@@ -73,8 +84,12 @@ namespace TicTacToe
             try
             {
                 Button2A.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button2A");
+                var checkWin = CheckForWin("2AX");
+                if (!checkWin)
+                {
+
+                    await OpponentsMove("Button2A");
+                }
 
             }
             catch (Exception ex)
@@ -89,8 +104,11 @@ namespace TicTacToe
             try
             {
                 Button2B.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button2B");
+                var checkWin = CheckForWin("2BX");
+                if (!checkWin)
+                {
+                    await OpponentsMove("Button2B");
+                }
 
             }
             catch (Exception ex)
@@ -105,8 +123,11 @@ namespace TicTacToe
             try
             {
                 Button2C.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button2C");
+                var checkWin = CheckForWin("2CX");
+                if (!checkWin)
+                {
+                    await OpponentsMove("Button2C");
+                }
 
             }
             catch (Exception ex)
@@ -122,8 +143,11 @@ namespace TicTacToe
             try
             {
                 Button3A.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button3A");
+                var checkWin = CheckForWin("3AX");
+                if (!checkWin)
+                {
+                    await OpponentsMove("Button3A");
+                }
 
             }
             catch (Exception ex)
@@ -138,8 +162,11 @@ namespace TicTacToe
             try
             {
                 Button3B.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button3B");
+                var checkWin = CheckForWin("3BX");
+                if (!checkWin)
+                {
+                    await OpponentsMove("Button3B");
+                }
 
             }
             catch (Exception ex)
@@ -154,8 +181,10 @@ namespace TicTacToe
             try
             {
                 Button3C.Content = CreateCrossImage();
-                CheckForWin();
-                await OpponentsMove("Button3C");
+                var checkWin = CheckForWin("3CX");
+                {
+                    await OpponentsMove("Button3C");
+                }
 
             }
             catch (Exception ex)
@@ -165,12 +194,47 @@ namespace TicTacToe
             }
         }
 
-        private void CheckForWin()
+        private bool CheckForWin(string move)
         {
+            Moves.Add(move);
 
+            if (Moves.Count > 2)
+            {
+                if (Moves.Contains("1AX") && Moves.Contains("1BX") && Moves.Contains("1CX")
+                    || Moves.Contains("2AX") && Moves.Contains("2BX") && Moves.Contains("2CX")
+                    || Moves.Contains("3AX") && Moves.Contains("3BX") && Moves.Contains("3CX")
 
+                    || Moves.Contains("1AX") && Moves.Contains("2AX") && Moves.Contains("3AX")
+                    || Moves.Contains("1BX") && Moves.Contains("2BX") && Moves.Contains("3BX")
+                    || Moves.Contains("1CX") && Moves.Contains("2CX") && Moves.Contains("3CX")
+
+                    || Moves.Contains("1AX") && Moves.Contains("2BX") && Moves.Contains("3CX")
+                    || Moves.Contains("3AX") && Moves.Contains("2BX") && Moves.Contains("1CX")
+                    )
+                {
+                    MessageBox.Show("You won!");
+                    return true;
+                }
+
+                else if (Moves.Contains("1AC") && Moves.Contains("1BC") && Moves.Contains("1CC")
+                    || Moves.Contains("2AC") && Moves.Contains("2BC") && Moves.Contains("2CC")
+                    || Moves.Contains("3AC") && Moves.Contains("3BC") && Moves.Contains("3CC")
+
+                    || Moves.Contains("1AC") && Moves.Contains("2AC") && Moves.Contains("3AC")
+                    || Moves.Contains("1BC") && Moves.Contains("2BC") && Moves.Contains("3BC")
+                    || Moves.Contains("1CC") && Moves.Contains("2CC") && Moves.Contains("3CC")
+
+                    || Moves.Contains("1AC") && Moves.Contains("2BC") && Moves.Contains("3CC")
+                    || Moves.Contains("3AC") && Moves.Contains("2BC") && Moves.Contains("1CC")
+                    )
+                {
+                    MessageBox.Show("The opponent won, how unfortunate!");
+                    return true;
+                }
+            }
+            return false;
             //check for three in a row
-            //if statement?
+
 
         }
 
@@ -194,45 +258,54 @@ namespace TicTacToe
                     case "Button1A":
                         AvailableCells.Remove("Button1A");
                         Button1A.Content = CreateCircleImage();
+                        CheckForWin("1AC");
                         break;
                     case "Button1B":
                         AvailableCells.Remove("Button1B");
                         Button1B.Content = CreateCircleImage();
+                        CheckForWin("1BC");
                         break;
                     case "Button1C":
                         AvailableCells.Remove("Button1C");
                         Button1C.Content = CreateCircleImage();
+                        CheckForWin("1CC");
                         break;
                     case "Button2A":
                         AvailableCells.Remove("Button2A");
                         Button2A.Content = CreateCircleImage();
+                        CheckForWin("2AC");
                         break;
                     case "Button2B":
                         AvailableCells.Remove("Button2B");
                         Button2B.Content = CreateCircleImage();
+                        CheckForWin("2BC");
                         break;
                     case "Button2C":
                         AvailableCells.Remove("Button2C");
                         Button2C.Content = CreateCircleImage();
+                        CheckForWin("2CC");
                         break;
                     case "Button3A":
                         AvailableCells.Remove("Button3A");
                         Button3A.Content = CreateCircleImage();
+                        CheckForWin("3AC");
                         break;
                     case "Button3B":
                         AvailableCells.Remove("Button3B");
                         Button3B.Content = CreateCircleImage();
+                        CheckForWin("3BC");
                         break;
                     case "Button3C":
                         AvailableCells.Remove("Button3C");
                         Button3C.Content = CreateCircleImage();
+                        CheckForWin("3CC");
                         break;
 
                     default:
                         break;
                 }
             }
-    
+
         }
 
         private List<string> InitializeAvailableCellsList()
@@ -243,6 +316,7 @@ namespace TicTacToe
 
         private void RestartGame(object sender, RoutedEventArgs e)
         {
+            Moves = new List<string>();
             AvailableCells = InitializeAvailableCellsList();
             Button1A.Content = "";
             Button1B.Content = "";
